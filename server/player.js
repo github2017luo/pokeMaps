283283002a53ad221cf434cd4b74fe43100c72f2
komali2 <rogersjcaleb@gmail.com>
2016-07-25 10:54:44 -0700

    core: implemented auth and getting a token

---
+++ server/player.js
@@ -0,0 +1,12 @@
+var player = {
+
+};
+
+player.token = null;
+
+player.api = null;
+player.latitude = 37.790251;
+player.longitude = -122.402933;
+player.altitude = 35.394;
+
+module.exports = player;
